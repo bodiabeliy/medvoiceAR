d0aa362fd87404c57f2b469c4a3b5523524d9075
bboda2106@gmail.com <bboda2106@gmail.com>
2026-06-24 19:35:44 +0300

    update files source url path

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ export const categoryTopicsList = [
   {
     isActive: true,
     image: ``,
-    imageTemplate: `https://medvoicear-server.onrender.com/banner/targetImage.mind`,
+    imageTemplate: `https://ar.medvoice.net/banner/targetImage.mind`,
     userExample: ``,
     topicName: "MedVoice",
     topicItems: [
@@ -10,9 +10,9 @@ export const categoryTopicsList = [
         parentTopic: "MedVoice",
         itemName: "MedVoice Video",
         targetIndex: 0,
-        type: "video",                                        // ← new video item
-        video: `https://medvoicear-server.onrender.com/banner/bannerVideo.mp4`,
-        greenScreen: true,                                    // ← removes green bg
+        type: "video",                                        
+        video: `https://ar.medvoice.net/banner/bannerVideo.mp4`,
+        greenScreen: true,                                   
       },
     ]
   }
